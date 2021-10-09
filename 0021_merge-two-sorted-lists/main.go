2d6e388667827fa5535a2610a6a8e67adfbdf546
Chris Byerly <c.byerly@techsmith.com>
2021-10-09 19:19:01 -0400

    add 21

---
+++ 0021_merge-two-sorted-lists/main.go
@@ -0,0 +1,32 @@
+// 21. Merge Two Sorted Lists
+// https://leetcode.com/problems/merge-two-sorted-lists/
+
+package main
+
+import (
+	"fmt"
+)
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+	if l1.Val < l2.Val {
+		l1.Next = mergeTwoLists(l1.Next, l2)
+		return l1
+	}
+	l2.Next = mergeTwoLists(l1, l2.Next)
+	return l2
+}
+
+func main() {
+	fmt.Println("lmao")
+}
