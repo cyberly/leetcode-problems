12d92c6b425baff7869de92f7a1e77fb42f5dd96
Chris Byerly <c.byerly@techsmith.com>
2021-10-13 17:55:06 -0400

    add 206

---
+++ 0609_find-duplicate-file-in-system/0206_reverse-linked-list/main.go
@@ -0,0 +1,37 @@
+// 206. Reverse Linked List
+// https://leetcode.com/problems/reverse-linked-list/
+
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	var prev *ListNode
+	curr := head
+	for curr != nil {
+		next := curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = next
+	}
+	return prev
+}
+
+func reverseList_recursion(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	p := reverseList(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+	return p
+}
+
+func main() {
+	fmt.Println("placeholder")
+}
