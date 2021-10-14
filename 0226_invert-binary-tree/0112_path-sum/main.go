2fd76bc98f6fc76e8881c9b5896347845a575606
Chris Byerly <c.byerly@techsmith.com>
2021-10-14 14:22:04 -0400

    add 112

---
+++ 0226_invert-binary-tree/0112_path-sum/main.go
@@ -0,0 +1,26 @@
+// 112. Path Sum
+// https://leetcode.com/problems/path-sum/
+
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func hasPathSum(root *TreeNode, targetSum int) bool {
+	if root == nil {
+		return false
+	}
+	if root.Val == targetSum && root.Left == nil && root.Right == nil {
+		return true
+	}
+	return hasPathSum(root.Left, targetSum-root.Val) || hasPathSum(root.Right, targetSum-root.Val)
+}
+
+func main() {
+	fmt.Println("lmoa")
+}
